b6b83665602016b65774221fcd1db7db98dfe0f9
xingyuefeng <15262870437@163.com>
2022-05-12 10:02:40 +0800

    feat(plugin-ts-type-doc): 完善插件

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# webpack开发工具箱
+# webpack5开发工具箱
 
 
 ## plugin-ts-type-doc
 
-将ts文件转成文档
+利用[typedoc](https://typedoc.org/) 将ts文件转成文档
 
 ### Installation
 
 > npm install --save-dev @maliao/plugin-ts-type-doc
 
+### Usage
 
 ```
 const PluginTsTypeDoc = require('@maliao/plugin-ts-type-doc')
@@ -16,10 +17,23 @@ const PluginTsTypeDoc = require('@maliao/plugin-ts-type-doc')
 
 {
  plugins: [
-    new PluginTsTypeDoc()
+    new PluginTsTypeDoc(options)
   ]
 
 }
 
 ```
 
+### options
+
+默认参数
+
+```
+{
+   entryPoints: ["src/index.ts"],
+   out: 'docs'
+}
+
+```
+
+继承自[TypeDocOptions](https://typedoc.org/api/modules.html#TypeDocOptions)
--- packages/plugin-ts-type-doc/src/index.ts
+++ packages/plugin-ts-type-doc/src/index.ts
@@ -1,56 +1,85 @@
 const TypeDoc = require('typedoc');
+import type { TypeDocOptions } from "typedoc";
+
 
 const pluginName = 'PluginTsTypeDoc';
 
 
+type Options  = Partial<TypeDocOptions>
+
 
 class PluginTsTypeDoc {
+
+  private options: Options;
+  private startTime =  Date.now();
+  private prevTimestamps = {};
+
+  constructor(options: Options) {
+
+    const defaultLoaderConfig: Options  = {
+      entryPoints: ["src/index.ts"]
+    }
+
+    if (!options?.out && !options?.json) {
+			defaultLoaderConfig.out = "./docs"
+		}
+
+    this.options =  Object.assign(options || {}, defaultLoaderConfig);
+  }
+
   apply(compiler) {
     compiler.hooks.emit.tapAsync(
       pluginName,
       (compilation, callback) => {
-        this.main()
+        this.main(compilation.fileSystemInfo._fileTshs)
+        // console.log(Object.entries(compilation.fileSystemInfo._fileTshs))
+       
         
         callback();
       }
     );
 
-
     compiler.hooks.done.tapAsync(pluginName, stats => {
-      console.log('编译完成')
+      console.log('Typedoc finished generating 🎉🎉🎉');
     })
 
   }
 
-  async main() {
-    const app = new TypeDoc.Application();
+  async main(fileTshs) {
+    const changedFiles = []
+    fileTshs.forEach((fileInfo, watchfile) => {
+      console.log(this.startTime, fileInfo.timestamp)
+      if (this.prevTimestamps[watchfile] || this.startTime < (fileInfo.timestamp || Infinity)) {
+        changedFiles.push(watchfile)
+      }
+    })
 
-    // If you want TypeDoc to load tsconfig.json / typedoc.json files
-    app.options.addReader(new TypeDoc.TSConfigReader());
-    app.options.addReader(new TypeDoc.TypeDocReader());
+    // 监听是否需要重新生成文档
+    if (changedFiles.length === 0) {
+      const app = new TypeDoc.Application();
 
-    app.bootstrap({
-        // typedoc options here
-        entryPoints: ["src/index.ts"],
-    });
-
-    const project = app.convert();
-
-    if (project) {
-        // Project may not have converted correctly
-        const outputDir = "docs";
-
-        // Rendered docs
-        await app.generateDocs(project, outputDir);
-        // Alternatively generate JSON output
-        await app.generateJson(project, outputDir + "/documentation.json");
+      app.options.addReader(new TypeDoc.TSConfigReader());
+      app.options.addReader(new TypeDoc.TypeDocReader());
+  
+      app.bootstrap(this.options);
+  
+      const project = app.convert();
+  
+      if (project) {
+          // Project may not have converted correctly
+          const outputDir = "docs";
+  
+          // Rendered docs
+          await app.generateDocs(project, outputDir);
+          // Alternatively generate JSON output
+          await app.generateJson(project, outputDir + "/documentation.json");
+      }
     }
+
+   
 }
 
 }
 
 
-
-
-
-module.exports = PluginTsTypeDoc;
+export default  PluginTsTypeDoc;
